3ab67fa446f81e97e5abe0986c3a0b9eb077f929
Adewale Orotayo <didymus7007@gmail.com>
2021-01-20 15:42:33 +0100

    Imported player laser into Player class Set condition for shooting in player update and also play a soud when shooting created player laser class that extends entity

--- src/Entities/Player.js
+++ src/Entities/Player.js
@@ -1,5 +1,6 @@
 // import Phaser from 'phaser';
 import Entity from './Entity';
+import PlayerLaser from './Weapons/PlayerLaser';
 
 export default class Player extends Entity {
   constructor(scene, x, y, key) {
@@ -29,6 +30,18 @@ export default class Player extends Entity {
   update() {
     this.body.setVelocity(0, 0);
     this.body.setCollideWorldBounds(true);
+
+    if (this.getData('isShooting')) {
+      if (this.getData('timerShootTick') < this.getData('timerShootDelay')) {
+        this.setData('timerShootTick', this.getData('timerShootTick') + 1);
+      } else {
+        const laser = new PlayerLaser (this.scene, this.x, this.y);
+        this.scene.playerLaser.add(laser);
+
+        this.scene.sfx.laser.play();
+        this.setData('timerShootTick', 0);
+      }
+    }
 
     // this.x = Phaser.Math.Clamp(this.x, 0, this.scene.game.config.width);
     // this.y = Phaser.Math.Clamp(this.y, 0, this.scene.game.config.height);
