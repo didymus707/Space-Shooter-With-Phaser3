3fc40bedf2875319f16a13fef7ab6cf9c927d057
Adewale Orotayo <didymus7007@gmail.com>
2021-01-23 12:14:45 +0100

    Set up highscores and posting to Api... (api is buggy at the moment

--- src/scenes/GameOverScene.js
+++ src/scenes/GameOverScene.js
@@ -1,6 +1,8 @@
 import Phaser from 'phaser';
 import config from '../config/config';
+import Button from '../Objects/Button';
 import ScrollingBackground from '../utilities/ScrollingBackground';
+import { postScores, getScores } from '../ScoreApi';
 
 export default class GameOver extends Phaser.Scene {
   constructor() {
@@ -8,7 +10,67 @@ export default class GameOver extends Phaser.Scene {
   }
 
   create() {
-    this.add.text(config.width / 2, config.height / 2, 'Game Over');
+    this.sfx = {
+      btnOver: this.sound.add('sndBtnOver'),
+      btnDown: this.sound.add('sndBtnDown'),
+    };
+
+    this.cameras.main.fadeIn(100, 0, 0, 0);
+
+    this.title = this.add.text(this.game.config.width * 0.5, 64, 'GAME OVER', {
+      fontFamily: 'monospace',
+      fontSize: 48,
+      fontStyle: 'bold',
+      color: '#ffffff',
+      align: 'center',
+    });
+    this.title.setOrigin(0.5);
+
+    this.highscoreLabel = this.add.text(this.game.config.width * 0.5, 128, `${this.sys.game.globals.playerName.toUpperCase()} SCORE: ${this.sys.game.globals.score}`, {
+      fontFamily: 'monospace',
+      fontSize: 32,
+      fontStyle: 'bold',
+      color: '#0f3',
+      align: 'center',
+    }).setOrigin(0.5);
+
+    postScores(this.sys.game.globals.playerName, this.sys.game.globals.score);
+
+    getScores().then(result => {
+      result.sort((a, b) => b.score - a.score)
+        .slice(0, 5)
+        .map((game, i) => {
+          const text = `Player: ${game.user.toUpperCase()} | Score: ${game.score}`;
+          this.add.text(config.width * 0.5, (85 * (i + 1.1)), text).setOrigin(0.5);
+          return text;
+        });
+    });
+
+    this.restartButton = this.add.sprite(config.width / 2, config.height / 2, 'normal').setInteractive();
+    this.restartText = this.add.text(config.width / 2, config.height / 2, 'Restart', { fontSize: '32px', fill: '#fff' });
+    Phaser.Display.Align.In.Center(this.restartText, this.restartButton);
+
+    this.restartButton.on('pointerover', () => {
+      this.restartButton.setTexture('hover');
+      this.sfx.btnOver.play();
+    });
+
+    this.restartButton.on('pointerout', () => {
+      this.restartButton.setTexture('normal');
+    });
+
+    this.restartButton.on('pointerdown', () => {
+      this.restartButton.setTexture('pressed');
+      this.cameras.main.fadeOut(1000, 0, 0, 0);
+      this.cameras.main.once(Phaser.Cameras.Scene2D.Events.FADE_OUT_COMPLETE, () => {
+        this.time.delayedCall(1000, () => {
+          this.scene.start('Game');
+        });
+      });
+    });
+
+    this.menuButton = new Button(this, config.width / 2, config.height / 2 + 100, 'normal', 'hover', 'pressed', 'Menu', 'Title', this.sfx.btnOver);
+
     this.gameBackgrounds = ['bg2', 'bg3', 'bg4'];
     this.backgrounds = [];
     for (let i = 0; i < 3; i += 1) {
